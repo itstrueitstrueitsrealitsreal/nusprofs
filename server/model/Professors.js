7a5d20cc129b4e4ada0c900ac3054db1c182dc47
itstrueitstrueitsrealitsreal <kenurfseet@gmail.com>
2024-01-20 23:08:53 +0800

    changed prof schema

--- server/model/Professors.js
+++ server/model/Professors.js
@@ -5,6 +5,21 @@ const ProfessorsSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    totalQuality: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    totalDifficulty: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    totalReviews: {
+        type: Number,
+        required: false,
+        default: 0
+    },
 })
 
 const Professors = mongoose.model('Professors', ProfessorsSchema);
