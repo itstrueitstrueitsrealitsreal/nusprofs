ba77456709b42742ac9d1bf2fb007dbc47369b05
itstrueitstrueitsrealitsreal <kenurfseet@gmail.com>
2024-01-20 14:36:41 +0800

    touched up database code

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	r := router.Setup()
-	fmt.Print("Listening on port 8000 at https://gossip-with-go.onrender.com!\n")
+	fmt.Print("Listening on port 8000 at http://localhost:8000!\n")
 
 	log.Fatalln(http.ListenAndServe(":8000", r))
 }
