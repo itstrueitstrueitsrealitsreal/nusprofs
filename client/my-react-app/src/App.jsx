2dfd65b4413bfffcee55908ee1c653cf404a8935
yeoshuheng <yeoshuheng@gmail.com>
2024-01-21 07:58:32 +0800

    chore: refactor client structure, removed redundant node_modules dir

--- client/my-react-app/src/App.jsx
+++ client/my-react-app/src/App.jsx
@@ -4,12 +4,16 @@ import './App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ReviewItem from './components/ReviewItem';
 import ProfessorReviewPage from './pages/ProfessorReviewPage';
+import { ProfessorForm } from './components/forms/ProfessorForm';
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <div className="app-container">
-      <TopNavBar />
+      <TopNavBar className="nav_main"/>
+      <div className="submit_review">
+        <ProfessorForm/>
+      </div>
     </div>
   )
 }
